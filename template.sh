720fab1a6d4b24e975df381329edf9e5b1cd1ac9
veerpal singh <veerpalsingh@veerpals-MacBook-Air.local>
2025-11-16 17:26:22 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src 
+mkdir -p research
+
+
+touch src/__init__.py
+touch scr/helper.py
+touch src/prompt.py
+touch .env
+touch research/
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and Files are created successfully!."
