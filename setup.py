3a1c602f96d8dc098ece0b48d087f974373f6684
veerpal singh <veerpalsingh@veerpals-MacBook-Air.local>
2025-11-16 20:51:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical-Chatbot',
+    version="0.1.0",
+    author="Veerpal Singh",
+    author_email="singhveerpal5102@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
